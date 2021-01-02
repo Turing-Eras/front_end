d4cedbdd6b8ecb991f1affb4f851e0baf3f6199b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-02 13:24:06 -0700

    Add type & click event

--- src/Components/Question.tsx
+++ src/Components/Question.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
+type ChangeQuestion = (index: number) => void
+
 type QuestionProps = {
+  changeQuestion: ChangeQuestion;
+  currentQuestionIndex: number;
   currentQuestion: string;
 }
+
 
 export const Question = (props: QuestionProps) => {
   return (
     <>
       <h1>{props.currentQuestion}</h1>
       <button type='button'>Skip</button>
-      <button type='button'>Next</button>
+      <button type='button' onClick={() => props.changeQuestion(props.currentQuestionIndex + 1)}>Next</button>
     </>
   );
 };
