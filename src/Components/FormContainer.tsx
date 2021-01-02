ed72ba93b7915864fdcbc8b9b7b1b01497a417fc
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-02 12:01:12 -0700

    Add form skeleton and pseudocode

--- src/Components/FormContainer.tsx
+++ src/Components/FormContainer.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+//Roles of buttons
+
+//Next
+//functions as submit
+
+//Skip
+//populates the answer with an empty string
+//counts as an answer to that question for progress bar
+
+//Back
+//maybe a stretch?
 
 export const FormContainer = () => {
-    return (
-        <h1>Form</h1>
-    )
-}
+  //index of question will be id
+
+  let questions; //pulled in from BE
+  useEffect(() => {
+    //set questions & store in questions variable
+  }, []);
+
+  const [answers, setAnswer] = useState([]);
+
+  let currentQuestion; //current index
+
+  return (
+    <>
+      <h1>Create My Calendar</h1>
+      <p>Question Goes Here</p>
+      <form>
+        <input type='date'></input>
+      </form>
+      <button type='button'>Skip</button>
+      <button type='button'>Next</button>
+    </>
+  );
+};
