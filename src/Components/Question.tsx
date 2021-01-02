a2e6ef32b16f923e081f22007088b4d6f2f077b7
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-02 12:16:51 -0700

    Change name & return buttons

---
+++ src/Components/Question.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Question = () => {
+  return (
+    <>
+      <button type='button'>Skip</button>
+      <button type='button'>Next</button>
+    </>
+  );
+};
