f23efb3cd21be45c8c70da215443684221b43989
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-06 15:10:56 -0700

    Add describe block with initial passing test

--- src/Components/NavBar/NavBar.test.tsx
+++ src/Components/NavBar/NavBar.test.tsx
@@ -2,3 +2,9 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { NavBar } from './NavBar';
+
+describe('NavBar', () => {
+    it('should render a NavBar', () => {
+      expect(true).toBe(true);
+    });
+  });
