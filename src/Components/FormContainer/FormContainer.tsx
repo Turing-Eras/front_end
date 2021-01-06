3e68aa9b8ccc6dec4f80bfcc0f7d33f28820cbda
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-06 15:59:31 -0700

    Install dependencies

--- src/Components/FormContainer/FormContainer.tsx
+++ src/Components/FormContainer/FormContainer.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Question } from '../Question/Question';
-import HeaderComponent from '../HeaderComponent/HeaderComponent'
+import { gql, useQuery } from '@apollo/client';
+import HeaderComponent from '../HeaderComponent/HeaderComponent';
+
 export const FormContainer = () => {
   let questions = ['who', 'what', 'where', 'when', 'why'];
   //const [questions, setQuestions] = useState([])
@@ -12,16 +14,21 @@ export const FormContainer = () => {
 
   const [answers, setAnswer] = useState<string[]>([]);
 
-  const [currentQuestionIndex, changeQuestion] = useState(0)
-  if(answers.length === questions.length ){
-    return <button type='submit'>Submit</button>
-
+  const [currentQuestionIndex, changeQuestion] = useState(0);
+  if (answers.length === questions.length) {
+    return <button type='submit'>Submit</button>;
   }
   return (
     <>
-      {answers.length === 0 && <HeaderComponent/>}
+      {answers.length === 0 && <HeaderComponent />}
       <form>
-        <Question currentQuestion={questions[currentQuestionIndex]} changeQuestion={changeQuestion} currentQuestionIndex={currentQuestionIndex} setAnswer={setAnswer} answers={answers}/>
+        <Question
+          currentQuestion={questions[currentQuestionIndex]}
+          changeQuestion={changeQuestion}
+          currentQuestionIndex={currentQuestionIndex}
+          setAnswer={setAnswer}
+          answers={answers}
+        />
       </form>
     </>
   );
