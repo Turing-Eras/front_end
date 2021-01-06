1427217783104c260c23516d41d56ad1e1111c26
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-05 19:42:30 -0700

    Write test for login form input and button

--- src/Components/LoginPage/LoginPage.test.tsx
+++ src/Components/LoginPage/LoginPage.test.tsx
@@ -5,7 +5,14 @@ import { render, screen } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
-  it('should render LoginPage', () => {
-    expect(true).toBe(true);
+  it('should render a form with text inputs and a button', () => {
+    const { getByPlaceholderText, getByText } = render(<LoginPage />);
+    const username = getByPlaceholderText('Username');
+    const password = getByPlaceholderText('Password');
+    const submitBtn = getByText('Submit');
+
+    expect(username).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(submitBtn).toBeInTheDocument();
   });
 });
