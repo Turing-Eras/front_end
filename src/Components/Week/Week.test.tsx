a26d7ed3feea12b83965d1f18105b63bf80eac94
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-06 15:13:53 -0700

    Add describe block, initial test

--- src/Components/Week/Week.test.tsx
+++ src/Components/Week/Week.test.tsx
@@ -2,3 +2,9 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { Week } from './Week';
+
+describe('Week', () => {
+    it('should render a Week componen', () => {
+      expect(true).toBe(true);
+    });
+  });
