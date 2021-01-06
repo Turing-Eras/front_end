6bf4536630d2718ba1352b61770acec6cb354012
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-05 20:00:06 -0700

    Write test for render of button & input

--- src/Components/Question/Question.test.tsx
+++ src/Components/Question/Question.test.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { getByPlaceholderText, render, screen } from '@testing-library/react';
 import { Question } from './Question';
 
 describe('Question', () => {
-  it('should render Question', () => {
-    expect(true).toBe(true);
+  it('should render with an input, a skip and a next button', () => {
+    const { getByTestId, getByText } = render(<Question />);
+
+    const dateInput = getByTestId('date');
+    const skipBtn = getByText('Skip');
+    const nextBtn = getByText('Next');
+
+    expect(dateInput).toBeInTheDocument();
+    expect(skipBtn).toBeInTheDocument();
+    expect(nextBtn).toBeInTheDocument();
   });
 });
