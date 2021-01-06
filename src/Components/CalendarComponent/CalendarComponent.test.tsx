e6beb89915dbb3c8afc725c432fc3c448cc88077
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-06 15:18:06 -0700

    Add describe block with initial passing test

--- src/Components/CalendarComponent/CalendarComponent.test.tsx
+++ src/Components/CalendarComponent/CalendarComponent.test.tsx
@@ -2,3 +2,9 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { CalendarComponent } from './CalendarComponent';
+
+describe('CalendarComponent', () => {
+    it('should render a CalendarComponent', () => {
+      expect(true).toBe(true);
+    });
+  });
