6fbffdb19a586da03c191e4377e3e0fbb773b8a2
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-05 19:47:33 -0700

    Create test for updating form

--- src/Components/LoginPage/LoginPage.test.tsx
+++ src/Components/LoginPage/LoginPage.test.tsx
@@ -7,12 +7,25 @@ import LoginPage from './LoginPage';
 describe('LoginPage', () => {
   it('should render a form with text inputs and a button', () => {
     const { getByPlaceholderText, getByText } = render(<LoginPage />);
-    const username = getByPlaceholderText('Username');
-    const password = getByPlaceholderText('Password');
+    const usernameInput = getByPlaceholderText('Username');
+    const passwordInput = getByPlaceholderText('Password');
     const submitBtn = getByText('Submit');
 
     expect(username).toBeInTheDocument();
     expect(password).toBeInTheDocument();
     expect(submitBtn).toBeInTheDocument();
+  });
+
+  it('should have empty text inputs and update upon user input', () => {
+    const { getByPlaceholderText } = render(<LoginPage />);
+
+    const usernameInput = getByPlaceholderText('Username');
+    const passwordInput = getByPlaceholderText('Password');
+
+    userEvent.type(usernameInput, 'user1');
+    userEvent.type(passwordInput, 'password1');
+
+    expect(usernameInput).toHaveValue('user1');
+    expect(passwordInput).toHaveValue('password1');
   });
 });
