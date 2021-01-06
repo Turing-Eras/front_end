793d1e2f92dbe1c9e5dac70a6b4e3644facd6200
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-05 22:06:46 -0700

    Fix spelling, remove console log

--- src/Components/CalendarComponent/CalendarComponent.tsx
+++ src/Components/CalendarComponent/CalendarComponent.tsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import Week from '../Week/Week'
-import NavBar from '../NavBar/NavBar'
-const CalenderComponent = () =>{
-  let calender = new Array(76)
-  calender.fill({})
-  console.log(calender)
-  let display = calender.map((year,index )=>{
-    let weeks = new Array (52)
-    weeks.fill( <Week/>)
-    return <section > 
-      Age: {index}
-      {weeks}
+import Week from '../Week/Week';
+import NavBar from '../NavBar/NavBar';
+
+const CalendarComponent = () => {
+  let calendar = new Array(76);
+  calendar.fill({});
+  let display = calendar.map((year, index) => {
+    let weeks = new Array(52);
+    weeks.fill(<Week />);
+    return (
+      <section>
+        Age: {index}
+        {weeks}
       </section>
-  })
+    );
+  });
   return (
     <section>
-      <NavBar/>
+      <NavBar />
       Your calender
       {display}
     </section>
-  )
-}
-export default CalenderComponent
+  );
+};
+export default CalendarComponent;
