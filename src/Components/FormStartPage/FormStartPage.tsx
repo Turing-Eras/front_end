b5d01c68867555825d7236ca8f626d7e9e92613a
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-07 14:37:08 -0700

    Fix routes

--- src/Components/FormStartPage/FormStartPage.tsx
+++ src/Components/FormStartPage/FormStartPage.tsx
@@ -3,20 +3,22 @@ import {Link} from 'react-router-dom'
 let FormStartPage = () =>{
   return (
     <section>
-      <p>
-        As you know life is filled with choices. 
-        We've decided to give you another.
-    You can either start off editend your calender from a blank calender or you can start off editing on a calender with some events.
-    If you choose the prefilled calender 
-      </p>
-      <button>
-        <Link to= '/calender'/>
-        Start Calender
-      </button>
-      <button>
-        <Link to= '/FormContainer'/>
-        Start Form
-      </button>
+        <p>
+          As you know life is filled with choices. 
+          We've decided to give you another.
+      You can either start off editend your calender from a blank calender or you can start off editing on a calender with some events.
+      If you choose the prefilled calender 
+        </p>
+          <Link to= '/calender'>
+        <button>
+          Start Calender
+        </button>
+        </Link>
+          <Link to= '/form'>
+            <button>
+              Start Form
+            </button>
+          </Link>
 
     </section>
   )
