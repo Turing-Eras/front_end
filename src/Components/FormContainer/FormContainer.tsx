045b278a040c2efe14c4144502341d1c043d7b29
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-06 19:26:20 -0700

    Write query for questions

--- src/Components/FormContainer/FormContainer.tsx
+++ src/Components/FormContainer/FormContainer.tsx
@@ -1,23 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { Question } from '../Question/Question';
 import { gql, useQuery } from '@apollo/client';
 import HeaderComponent from '../HeaderComponent/HeaderComponent';
 
+interface FormContainer {
+  useEffect: void;
+}
+
 export const FormContainer = () => {
-  let questions = ['who', 'what', 'where', 'when', 'why'];
+  // let questions = ['who', 'what', 'where', 'when', 'why'];
   //const [questions, setQuestions] = useState([])
 
+  const [questions, setState] = React.useState([]);
+
+  let query = gql`
+    {
+      getOnboardingQuestions {
+        id
+        question
+        name
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(query);
+
+  let message;
+
   useEffect(() => {
-    //set questions & store in questions variable
-    //setQuestions(returnBE)
-  }, []);
+    if (loading) {
+      message = 'Loading...';
+    }
+    if (error) {
+      message = error;
+    }
+    // do some checking here to ensure data exist
+    else if (data) {
+      message = '';
+      // mutate data if you need to
+      console.log(data);
+      setState(data);
+    }
+  }, [data]);
 
   const [answers, setAnswer] = useState<string[]>([]);
 
   const [currentQuestionIndex, changeQuestion] = useState(0);
+
+  if (message) {
+    return <h1>{message}</h1>;
+  }
+
   if (answers.length === questions.length) {
     return <button type='submit'>Submit</button>;
   }
+
   return (
     <>
       {answers.length === 0 && <HeaderComponent />}
