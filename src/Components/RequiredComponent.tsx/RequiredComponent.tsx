65dcdb2ebf5194ffe7bac85219a13abc291aa65a
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-06 20:18:42 -0700

    Add return value to Required form

--- src/Components/RequiredComponent.tsx/RequiredComponent.tsx
+++ src/Components/RequiredComponent.tsx/RequiredComponent.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 let RequiredForm = () =>{
 let questions = ['What is your name', 'What is your birthday', 'What is your email address']
+let [currentQuestionIndex,updateIndex ]= useState(0)
 return(
   <form>
-    <TextQuestion let currentQuestion = {}/>
+    <TextQuestion let currentQuestion = {questions[currentQuestionIndex]}/>
     </form>
 )
 }
