b52c4cf30333a83b8ec4317818f26ae9cc49d441
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-06 20:13:06 -0700

    Add initial requiredForm

---
+++ src/Components/RequiredComponent.tsx/RequiredComponent.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+let RequiredForm = () =>{
+let questions = ['What is your name', 'What is your birthday', 'What is your email address']
+return(
+  <form>
+    <TextQuestion let currentQuestion = {}/>
+    </form>
+)
+}
