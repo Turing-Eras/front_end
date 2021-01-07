571dcaf539be9d6ddb2b7605930d191320e27982
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-06 19:53:27 -0700

    Add test for title

--- src/Components/CalendarComponent/CalendarComponent.test.tsx
+++ src/Components/CalendarComponent/CalendarComponent.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import { CalendarComponent } from './CalendarComponent';
+import CalendarComponent from './CalendarComponent';
 
 describe('CalendarComponent', () => {
-    it('should render a CalendarComponent', () => {
-      expect(true).toBe(true);
-    });
+  it('should render a CalendarComponent with a title', () => {
+    const { getByText } = render(<CalendarComponent />);
+
+    const title = getByText('Your calendar');  
   });
+});
