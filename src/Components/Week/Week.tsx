49d380f68c30b1c905abc0e232847f41b60dbdc3
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-08 18:26:39 -0700

    Fix merge conflict

--- src/Components/Week/Week.tsx
+++ src/Components/Week/Week.tsx
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import './Week.css';
 
+
 const Week = () => {
   let handleClick = () => {
     
   };
-  return <section className='week' onClick={handleClick}></section>;
+  let [color, changeColor] = useState('red')
+
+  const handleDragOver = (e : React.DragEvent<HTMLElement>) => {
+    console.log('Ive been dragged')
+    changeColor('blue')
+    e.preventDefault();
+    e.stopPropagation();
+  }; 
+  const handleDragEnter = (e :React.DragEvent<HTMLElement>) =>{
+    console.log('Start dragging me')
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  return <section style = {{backgroundColor: color}} className='week' onClick={handleClick} onDragEnter={e => handleDragEnter(e)}
+  onDragOver = {e =>{handleDragOver(e)}}></section>;
 };
 
 export default Week;
