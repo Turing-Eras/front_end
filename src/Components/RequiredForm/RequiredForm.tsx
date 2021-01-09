7269270b27b4d8fa158b9b61d8149c032b165072
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-08 17:05:28 -0700

    Minor changes

--- src/Components/RequiredForm/RequiredForm.tsx
+++ src/Components/RequiredForm/RequiredForm.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
 import TextQuestion from '../TextQuestion/TextQuestion'
 import FormStartPage from '../FormStartPage/FormStartPage'
+import { gql, useMutation } from '@apollo/client';
+
 let RequiredForm = () =>{
+  let create_user = gql `
+   mutation {
+    createUser(input:{
+      name:"John",
+      email:"John@gmail.co",
+      birthdate:"1987/7/17"
+    }){
+      name,
+      email,
+      birthdate
+    }
+  }
+  `
 let questions = ['What is your name', 'What is your birthday', 'What is your email address']
 let [answers,addAnswer ]= useState<string[]>([])
 let [currentQuestionIndex,updateIndex ]= useState(0)
 if(answers.length ===questions.length  ){
   return <FormStartPage/>
 }
+console.log(answers)
 return(
 <form>
   User message instructions will go here
