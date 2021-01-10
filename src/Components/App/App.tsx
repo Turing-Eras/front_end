a40c130b85e2bb5fb627cc84348f9a25c1c2b8f5
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-09 23:03:19 -0700

    Fix typos

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import { FormContainer } from '../FormContainer/FormContainer';
-import {Route} from 'react-router-dom'
-import LoginPage from '../LoginPage/LoginPage'
-import CalendarComponent from '../CalendarComponent/CalendarComponent'  
-import RequiredForm from '../RequiredForm/RequiredForm'
+import React from "react";
+import { FormContainer } from "../FormContainer/FormContainer";
+import { Route } from "react-router-dom";
+import LoginPage from "../LoginPage/LoginPage";
+import CalendarComponent from "../CalendarComponent/CalendarComponent";
+import RequiredForm from "../RequiredForm/RequiredForm";
 export const App = () => {
-  return(
-  <>
-    <Route exact path='/'>
-        <RequiredForm/> 
+  return (
+    <>
+      <Route exact path="/">
+        <RequiredForm />
       </Route>
-      
-      <Route path = '/login' 
-        render = {() =>{
-          return <LoginPage />
-        }}>
-      </Route>
-      <Route path = '/form' 
-        render = {() =>{
-          return <FormContainer />
-        }}>
-      </Route>
-      <Route path = '/calender' 
-        render = {() =>{
-          return <CalendarComponent />
-        }}>
-      </Route>
-  </>
-  )
-}
 
+      <Route
+        path="/login"
+        render={() => {
+          return <LoginPage />;
+        }}
+      ></Route>
+      <Route
+        path="/form"
+        render={() => {
+          return <FormContainer />;
+        }}
+      ></Route>
+      <Route
+        path="/calendar"
+        render={() => {
+          return <CalendarComponent />;
+        }}
+      ></Route>
+    </>
+  );
+};
