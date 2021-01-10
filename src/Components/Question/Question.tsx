65b707cdc83bda5e4c4ac061356e79f308f8e42a
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-09 21:48:39 -0700

    Add conditional to create two endputs for era questions

--- src/Components/Question/Question.tsx
+++ src/Components/Question/Question.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
 
 type ChangeQuestion = (index: number) => void;
 
@@ -16,42 +17,56 @@ type QuestionProps = {
 
 export const Question = (props: QuestionProps) => {
   const [date, updateDate] = useState("");
+  const [endDate, updateEndDate] = useState("");
   const [answer, saveAnswer] = useState("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     updateDate(event.target.value);
     saveAnswer(event.target.value);
   };
-  if (props.currentQuestion.event_type === "event")
-    return (
-      <>
-        <h1>{props.currentQuestion}</h1>
+  let mutation;
+  if (props.questionType === "event") {
+    mutation = gql``;
+  }
+  if (props.questionType === "era") {
+  }
+  return (
+    <>
+      <h1>{props.currentQuestion}</h1>
+      <input
+        data-testid="date"
+        type="date"
+        onChange={handleChange}
+        value={date}
+      ></input>
+      {props.questionType === "event" && (
         <input
           data-testid="date"
           type="date"
           onChange={handleChange}
           value={date}
         ></input>
-        <button
-          type="button"
-          onClick={() => {
-            props.changeQuestion(props.currentQuestionIndex + 1);
-            props.setAnswer([...props.answers, ""]);
-            updateDate("");
-          }}
-        >
-          Skip
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            props.changeQuestion(props.currentQuestionIndex + 1);
-            props.setAnswer([...props.answers, answer]);
-            updateDate("");
-          }}
-        >
-          Next
-        </button>
-      </>
-    );
+      )}
+      <button
+        type="button"
+        onClick={() => {
+          props.changeQuestion(props.currentQuestionIndex + 1);
+          props.setAnswer([...props.answers, ""]);
+          updateDate("");
+        }}
+      >
+        Skip
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          props.changeQuestion(props.currentQuestionIndex + 1);
+          props.setAnswer([...props.answers, answer]);
+          updateDate("");
+        }}
+      >
+        Next
+      </button>
+    </>
+  );
 };
