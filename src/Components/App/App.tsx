eb9569932be5036596250ba0c69241c3a176e506
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-09 21:29:11 -0700

    Add userId state to app

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,32 +1,39 @@
-import React from 'react';
-import { FormContainer } from '../FormContainer/FormContainer';
-import {Route} from 'react-router-dom'
-import LoginPage from '../LoginPage/LoginPage'
-import CalendarComponent from '../CalendarComponent/CalendarComponent'  
-import RequiredForm from '../RequiredForm/RequiredForm'
+import React, { useState } from "react";
+import { FormContainer } from "../FormContainer/FormContainer";
+import { Route } from "react-router-dom";
+import LoginPage from "../LoginPage/LoginPage";
+import CalendarComponent from "../CalendarComponent/CalendarComponent";
+import RequiredForm from "../RequiredForm/RequiredForm";
 export const App = () => {
-  return(
-  <>
-    <Route exact path='/'>
-        <RequiredForm/> 
-      </Route>
-      
-      <Route path = '/login' 
-        render = {() =>{
-          return <LoginPage />
-        }}>
-      </Route>
-      <Route path = '/form' 
-        render = {() =>{
-          return <FormContainer />
-        }}>
-      </Route>
-      <Route path = '/calender' 
-        render = {() =>{
-          return <CalendarComponent />
-        }}>
-      </Route>
-  </>
-  )
-}
+  let [userId, updateUserId] = useState();
+  return (
+    <>
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return <RequiredForm updateUserId={updateUserId} />;
+        }}
+      ></Route>
 
+      <Route
+        path="/login"
+        render={() => {
+          return <LoginPage />;
+        }}
+      ></Route>
+      <Route
+        path="/form"
+        render={() => {
+          return <FormContainer />;
+        }}
+      ></Route>
+      <Route
+        path="/calender"
+        render={() => {
+          return <CalendarComponent />;
+        }}
+      ></Route>
+    </>
+  );
+};
