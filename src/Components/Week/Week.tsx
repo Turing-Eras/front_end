7f5cbdff3df30e744bcffa3eb6ac392a17d895a0
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-11 10:50:21 -0700

    Add onclick event to week component

--- src/Components/Week/Week.tsx
+++ src/Components/Week/Week.tsx
@@ -4,14 +4,23 @@ import './Week.css';
 type WeekProps = {
   key: number;
   index: number;
+  color: string | null;
+  name: string;
 };
 
 const Week = (props: WeekProps) => {
+  let color;
+  if(props.color === null){
+    color = 'purple'
+  }
+  else{
+    color = props.color
+  }
   let handleClick = () => {
-
+    console.log('you clicked', props.name)
   };
 
-  return <section className='week' onClick={handleClick}></section>;
+  return <section style = {{backgroundColor:color}} className='week' onClick={handleClick}></section>;
 };
 
 export default Week;
