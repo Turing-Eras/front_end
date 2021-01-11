d7a768e36a6d7192e12c49098b4c11f17a81fb8c
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-10 19:52:10 -0700

    Add describe block with initial passing test

--- src/Components/TextQuestion/TextQuestion.test.tsx
+++ src/Components/TextQuestion/TextQuestion.test.tsx
@@ -3,3 +3,9 @@ import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
 import { TextQuestion } from './TextQuestion';
+
+describe('TextQuestion', () => {
+    it('should render a TextQuestion component', () => {
+      expect(true).toBe(true);
+    });
+  }); 
