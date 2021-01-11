52bf816a4eeebc9e82b08d2f407a42c69eabf61e
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-11 16:44:25 -0700

    Remove console log, format with Prettier

--- src/Components/CalendarComponent/CalendarComponent.tsx
+++ src/Components/CalendarComponent/CalendarComponent.tsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from "react";
-import Week from "../Week/Week";
-import NavBar from "../NavBar/NavBar";
-import { gql, useQuery } from "@apollo/client";
+import React, { useEffect, useState } from 'react';
+import Week from '../Week/Week';
+import NavBar from '../NavBar/NavBar';
+import { gql, useQuery } from '@apollo/client';
 
 type CalenderComponentProps = {
   userId: number;
 };
 type event = {
-  color: string | null,
-  date: string,
-  id: string,
-  name: string,
-  weekNumber: number
-}
+  color: string | null;
+  date: string;
+  id: string;
+  name: string;
+  weekNumber: number;
+};
 type era = {
-  color: string | null,
-  startWeek: number,
-  endWeek: number,
-  id: string,
-  name: string,
-  weekNumber: number
-}
-const CalendarComponent = (props: CalenderComponentProps) => {
-  let Get_User = gql`
-  query getUser($id :ID!){
-    getUser(id:$id){
+  color: string | null;
+  startWeek: number;
+  endWeek: number;
+  id: string;
+  name: string;
+  weekNumber: number;
+};
+
+export const Get_User = gql`
+  query getUser($id: ID!) {
+    getUser(id: $id) {
       id
       name
       events {
@@ -34,7 +34,7 @@ const CalendarComponent = (props: CalenderComponentProps) => {
         weekNumber
         color
       }
-      eras{
+      eras {
         id
         name
         startWeek
@@ -43,9 +43,11 @@ const CalendarComponent = (props: CalenderComponentProps) => {
       }
     }
   }
-  `;
+`;    
+
+const CalendarComponent = (props: CalenderComponentProps) => {
   const { data, loading, error } = useQuery(Get_User, {
-    variables: { id: props.userId },
+    variables: { id: props.userId }
   });
   if (loading) {
     return <p>Loading your Calender</p>;
@@ -53,44 +55,61 @@ const CalendarComponent = (props: CalenderComponentProps) => {
   if (error) {
     return <p>SOMETHING WENT WRONG</p>;
   }
-  if(data){
-    console.log(data)
+  if (data) {
+             
   }
   // useMemo() for the fills
   let calendar = new Array(76);
   calendar.fill({});
   let display = calendar.map((year, index) => {
     let weeks = new Array(52);
-    weeks.fill("");
+    weeks.fill('');
     let weeksDisplay = weeks.map((week, i) => {
       let currentWeek = i + 1;
       if (index > 0) {
         currentWeek = i + 1 + index * 52;
       }
-      let currentEvent = data.getUser.events.find((event:event) =>{
-        return event.weekNumber === currentWeek
-      })
-      let currentEra = data.getUser.eras.find((era:era )=>{
-        console.log(era.startWeek === currentWeek)
-        if(era.startWeek >= currentWeek && era.endWeek <=currentWeek){
-          console.log(era.startWeek)
-          console.log(era.endWeek)
+      let currentEvent = data.getUser.events.find((event: event) => {
+        return event.weekNumber === currentWeek;
+      });
+      let currentEra = data.getUser.eras.find((era: era) => {
+        if (era.startWeek >= currentWeek && era.endWeek <= currentWeek) {
+          
         }
 
-        if(era.startWeek >= currentWeek){
-          return true
+        if (era.startWeek >= currentWeek) {
+          return true;
         }
-        if(currentWeek <= era.endWeek )
-        return false
-      })
-      if(currentEvent){
-        return <Week key={currentWeek} index={currentWeek} color = {currentEvent.color} name = {currentEvent.name} />;
+        if (currentWeek <= era.endWeek) return false;
+      });
+      if (currentEvent) {
+        return (
+          <Week
+            key={currentWeek}
+            index={currentWeek}
+            color={currentEvent.color}
+            name={currentEvent.name}
+          />
+        );
       }
-      if(currentEra){
-        return <Week key={currentWeek} index={currentWeek} color = {currentEra.color} name = {currentEra.name} />;
+      if (currentEra) {
+        return (
+          <Week
+            key={currentWeek}
+            index={currentWeek}
+            color={currentEra.color}
+            name={currentEra.name}
+          />
+        );
       }
-      return <Week key={currentWeek} index={currentWeek} color = {'none'} name = {null} />;
-
+      return (
+        <Week
+          key={currentWeek}
+          index={currentWeek}
+          color={'none'}
+          name={null}
+        />
+      );
     });
 
     return (
@@ -110,5 +129,5 @@ const CalendarComponent = (props: CalenderComponentProps) => {
   );
 };
 // let shouldRerender = (prevProps,nextProps) =>{
-  // if()}
+// if()}
 export default React.memo(CalendarComponent);
