14d8abca71478d94f3ede3de62061dd8577b9f22
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-10 19:54:06 -0700

    Add describe block, initial test

--- src/Components/FormStartPage/FormStartPage.test.tsx
+++ src/Components/FormStartPage/FormStartPage.test.tsx
@@ -3,3 +3,9 @@ import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
 import { FormStartPage } from './FormStartPage';
+
+describe('FormStartPage', () => {
+    it('should render a FormStartPage component', () => {
+      expect(true).toBe(true);
+    });
+  });   
