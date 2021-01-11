e6e8a82f480e90b41c8f80eb37528f91d7880eba
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-10 19:53:04 -0700

    Add describe block and initial test

--- src/Components/RequiredForm/RequiredForm.test.tsx
+++ src/Components/RequiredForm/RequiredForm.test.tsx
@@ -3,3 +3,9 @@ import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
 import { RequiredForm } from './RequiredForm';
+
+describe('RequiredForm', () => {
+  it('should render a RequiredForm component', () => {
+    expect(true).toBe(true);
+  });  
+});
