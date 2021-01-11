4d5267e6713597c9873264cb20add02ca02631e3
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-10 19:52:34 -0700

    Add RequiredForm Test file with initial imports

---
+++ src/Components/RequiredForm/RequiredForm.test.tsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor } from '@testing-library/react';
+import { RequiredForm } from './RequiredForm';
