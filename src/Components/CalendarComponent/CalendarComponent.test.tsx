d3e6e92ca6ab242c2dffeb2c6b22b7d3c529e927
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-11 18:20:44 -0700

    Write test for loading message

--- src/Components/CalendarComponent/CalendarComponent.test.tsx
+++ src/Components/CalendarComponent/CalendarComponent.test.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import {
+  getByPlaceholderText,
+  render,
+  screen,
+  waitFor
+} from '@testing-library/react';
 import CalendarComponent from './CalendarComponent';
 import { MockedProvider } from '@apollo/client/testing';
-import { Get_User } from './CalendarComponent';     
+import { Get_User } from './CalendarComponent';
 
 const mocks = [
   {
     request: {
-      query: Get_User,   
+      query: Get_User,
       variables: { id: 1 }
     },
     result: {
@@ -43,19 +48,30 @@ const mocks = [
 describe('CalendarComponent', () => {
   it('should render a CalendarComponent with a title and ages', async () => {
     const { getByText } = render(
-      <MockedProvider mocks={mocks}>      
-        <CalendarComponent userId={1}/>     
+      <MockedProvider mocks={mocks}>
+        <CalendarComponent userId={1} />
       </MockedProvider>
-    );              
+    );
 
-    const title = await waitFor(() => getByText('Your calendar'))
-    const age0 = await waitFor(() => getByText('Age: 0'))
-    const age30 = await waitFor(() => getByText('Age: 30'))
-    const age60 = await waitFor(() => getByText('Age: 60'))
-   
+    const title = await waitFor(() => getByText('Your calendar'));
+    const age0 = await waitFor(() => getByText('Age: 0'));
+    const age30 = await waitFor(() => getByText('Age: 30'));
+    const age60 = await waitFor(() => getByText('Age: 60'));
+
     expect(title).toBeInTheDocument();
     expect(age0).toBeInTheDocument();
     expect(age30).toBeInTheDocument();
-    expect(age60).toBeInTheDocument();      
-  });      
-});    
+    expect(age60).toBeInTheDocument();
+  });
+
+  it('should render a loading message', () => {
+    const { getByText } = render(
+      <MockedProvider>
+        <CalendarComponent />
+      </MockedProvider>
+    );
+
+    const loadingMsg = getByText('Loading your Calender');
+    expect(loadingMsg).toBeInTheDocument();
+  });
+});
