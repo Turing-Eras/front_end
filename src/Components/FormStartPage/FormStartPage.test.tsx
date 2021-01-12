7bfaf8508434f90314b782a209852bb28351ce4f
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-11 21:14:08 -0700

    Add test for buttons

--- src/Components/FormStartPage/FormStartPage.test.tsx
+++ src/Components/FormStartPage/FormStartPage.test.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
-import { render, screen, waitFor } from '@testing-library/react';
+import { getByTestId, render, screen, waitFor } from '@testing-library/react';
 import FormStartPage from './FormStartPage';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -18,6 +18,23 @@ describe('FormStartPage', () => {
     const instructions = getByText(
       "As you know life is filled with choices. We've decided to give you another. You can start off editing a blank calendar by selecting 'Start Calendar' or you can select 'Start Form' to answer a few questions that will create events for you. You can add more events later if you choose!"
     );
+
     expect(instructions).toBeInTheDocument();
   });
+
+  it('should display Start Calendar and Start Form buttons', () => {
+    const mockUpdateUser = jest.fn();
+
+    const { getByText } = render(
+      <BrowserRouter>
+        <FormStartPage updateUserId={mockUpdateUser} />
+      </BrowserRouter>
+    );
+
+    const startCalendarBtn = getByText('Start Calendar');
+    const startFormBtn = getByText('Start Form');
+
+    expect(startCalendarBtn).toBeInTheDocument();
+    expect(startFormBtn).toBeInTheDocument();
+  });  
 });
