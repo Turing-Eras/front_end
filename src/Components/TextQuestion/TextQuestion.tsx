0d0cdac45682265a375fd38505be8b8e4e10f577
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-11 21:29:24 -0700

    Clean up code formatting

--- src/Components/TextQuestion/TextQuestion.tsx
+++ src/Components/TextQuestion/TextQuestion.tsx
@@ -11,36 +11,40 @@ type QuestionProps = {
   answers: string[];
 };
 
-let TextQuestion = (props:QuestionProps) =>{
-  const [text, updateText] = useState('')
+let TextQuestion = (props: QuestionProps) => {
+  const [text, updateText] = useState('');
   const [answer, saveAnswer] = useState('');
 
-  const handleChange =  (event: React.ChangeEvent<HTMLInputElement>) => {
-    updateText(event.target.value)
-    saveAnswer(event.target.value)
-
-    
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateText(event.target.value);
+    saveAnswer(event.target.value);
   };
-  let type = 'text'
-  if(props.currentQuestionIndex === 1){
-    type = 'date'
+  let type = 'text';
+  if (props.currentQuestionIndex === 1) {
+    type = 'date';
   }
   return (
     <section>
       <h1>{props.currentQuestion}</h1>
-      <input data-testid='text' type={type} onChange={handleChange} value = {text} ></input>
-      {text !== '' && <button
-        type='button'
-        onClick={() => {
-          props.changeQuestion(props.currentQuestionIndex + 1);
-          props.addAnswer([...props.answers, answer]);
-          updateText('')
-        }}>
-        Next
-      </button>
-      }
+      <input
+        data-testid='text'
+        type={type}
+        onChange={handleChange}
+        value={text}
+      ></input>
+      {text !== '' && (
+        <button
+          type='button'
+          onClick={() => {
+            props.changeQuestion(props.currentQuestionIndex + 1);
+            props.addAnswer([...props.answers, answer]);
+            updateText('');
+          }}
+        >
+          Next
+        </button>
+      )}
     </section>
-    
-  )
-}
-export default TextQuestion
+  );
+};
+export default TextQuestion;
