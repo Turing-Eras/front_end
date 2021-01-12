330a2e463e0dd6bef571049918dc7c67c051da20
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-11 21:07:31 -0700

    Write test for displaying user instructions

--- src/Components/FormStartPage/FormStartPage.test.tsx
+++ src/Components/FormStartPage/FormStartPage.test.tsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
-import { FormStartPage } from './FormStartPage';
+import FormStartPage from './FormStartPage';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('FormStartPage', () => {
-  it('should render a FormStartPage component', () => {
-    expect(true).toBe(true);
+  it('should render a FormStartPage with instructions', () => {
+    const mockUpdateUser = jest.fn();
+
+    const { getByText } = render(
+      <BrowserRouter>
+        <FormStartPage updateUserId={mockUpdateUser} />
+      </BrowserRouter>
+    );
+
+    const instructions = getByText(
+      "As you know life is filled with choices. We've decided to give you another. You can start off editing a blank calendar by selecting 'Start Calendar' or you can select 'Start Form' to answer a few questions that will create events for you. You can add more events later if you choose!"
+    );
+    expect(instructions).toBeInTheDocument();
   });
 });
