dc2f3b6128fff80daf4c5c4cdf8bc98a9022ddb9
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-11 21:28:31 -0700

    Write test for rendering question

--- src/Components/TextQuestion/TextQuestion.test.tsx
+++ src/Components/TextQuestion/TextQuestion.test.tsx
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
-import { TextQuestion } from './TextQuestion';
+import TextQuestion from './TextQuestion';
 
 describe('TextQuestion', () => {
-    it('should render a TextQuestion component', () => {
-      expect(true).toBe(true);
-    });
-  }); 
+  it('should render the current question', () => {
+    const questions = [
+      'What is your name?',
+      'What is your birthday?',
+      'What is your email address?'
+    ];
+
+    const { getByText } = render(
+      <TextQuestion currentQuestion={questions[0]} />
+    );
+
+    const question = getByText('What is your name?');
+    expect(question).toBeInTheDocument();
+  });     
+});
