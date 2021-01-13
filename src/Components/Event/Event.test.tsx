8ac3a586e182f06b2951aaf097c4fb1cda923433
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-12 18:18:52 -0700

    Write test for add an era

--- src/Components/Event/Event.test.tsx
+++ src/Components/Event/Event.test.tsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
-import { getByText, render, screen, waitFor } from '@testing-library/react';
+import {
+  getByTestId,
+  getByText,
+  render,
+  screen,
+  waitFor
+} from '@testing-library/react';
 import Event from './Event';
 
 describe('Event', () => {
+  it('should render a button with the correct text', () => {
+    const { getByText } = render(<Event />);
 
-  it('should render the component', () => {
-    const { getByText } = render(
-      <Event />
-    );
-
-    expect(true).toBe(true)
+    const addEraBtn = getByText('Add an Era');
+    expect(addEraBtn).toBeInTheDocument();
   });
 });
