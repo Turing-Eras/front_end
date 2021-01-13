cc07643d30563b10f56fd5538624d18907191201
Katy St Sauveur <katyrogowski@yahoo.com>
2021-01-12 18:11:18 -0700

    Logo added to landing page

--- src/Components/HeaderComponent/HeaderComponent.tsx
+++ src/Components/HeaderComponent/HeaderComponent.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './HeaderComponent.css';
+import Logo from '../../../public/eras-logo-horizontal.png'
 
 
 const HeaderComponent = () =>{
   return (
       <header className='header'>
-        {/* <img src = ''> logo*/}
-
+        <img src='eras-logo-horizontal.png' className='logo'/>
         <div>
-        <h1 className='logo-area'>ERAS</h1>
+        {/*<h1 className='logo-area'>ERAS</h1> */}
         </div>
         <div className='active-buttons'>
-          {/*<NavLink className='login-button header-link header-text' to="/login">Login</NavLink> */}
+          {/* <NavLink className='login-button header-link header-text' to="/login">Login</NavLink> */}
           {/* <NavLink className='calender-button header-link header-text' to="/calender">Calendar</NavLink> */}
         </div>
       </header>
