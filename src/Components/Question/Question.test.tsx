8f74ec46c0510fce7412a2a55a6fe1f9c0360f98
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-12 18:01:12 -0700

    Fix failing test after merge

--- src/Components/Question/Question.test.tsx
+++ src/Components/Question/Question.test.tsx
@@ -6,9 +6,6 @@ import { Question } from './Question';
 import { figureMutation, createEvent } from './Question';
 import { MockedProvider } from '@apollo/client/testing';
 
-// import TestRenderer from 'react-test-renderer';
-import { DocumentNode, gql, useMutation } from '@apollo/client';
-
 const mocks = [
   {
     request: {
@@ -47,11 +44,11 @@ describe('Question', () => {
 
     const dateInput = getByTestId('date');
     const skipBtn = getByText('Skip');
-    // const nextBtn = getByText('Next');
+    const nextBtn = getByText('Next');
 
     expect(dateInput).toBeInTheDocument();
     expect(skipBtn).toBeInTheDocument();
-    // expect(nextBtn).toBeInTheDocument();
+    expect(nextBtn).toBeInTheDocument();
   });
 
   it('should display the current question', () => {
@@ -110,7 +107,7 @@ describe('Question', () => {
     expect(dateInput.value).toEqual(testDate);
   });
 
-  it.skip('should fire a function when the next button is clicked', () => {
+  it('should fire a function when the next button is clicked', () => {
     const mockQuestions = [
       {
         question: 'When did you graduate high school?',
@@ -138,14 +135,22 @@ describe('Question', () => {
       </MockedProvider>
     );
 
+    const testDate = '2021-01-05';
     const nextBtn = getByText('Next');
+    const dateInput = getByTestId('date');   
     const question = getByText('When did you graduate high school?');
+
     expect(question).toBeInTheDocument();
-
-    userEvent.click(nextBtn);
-
+    expect(dateInput).toBeInTheDocument();
+    expect(nextBtn).toBeInTheDocument();  
+    
+    expect(dateInput.value).toEqual('');
+    fireEvent.change(dateInput, { target: { value: testDate } });
+    expect(dateInput.value).toEqual(testDate);
+    userEvent.click(nextBtn);     
+    
     expect(mockChangeQ).toHaveBeenCalledTimes(1);
-    expect(mockSetAnswer).toHaveBeenCalledTimes(1);
+    expect(mockSetAnswer).toHaveBeenCalledTimes(1); 
   });
 
   it('should fire a function when the skip button is clicked', () => {
@@ -162,7 +167,7 @@ describe('Question', () => {
 
     const mockChangeQ = jest.fn();
     const mockSetAnswer = jest.fn();
-    const mockAnswers = [];    
+    const mockAnswers = [];
 
     const { getByTestId, getByText } = render(
       <MockedProvider mocks={[]}>
@@ -174,13 +179,13 @@ describe('Question', () => {
           answers={mockAnswers}
         />
       </MockedProvider>
-    );   
+    );
 
     const skipBtn = getByText('Skip');
 
     userEvent.click(skipBtn);
 
     expect(mockChangeQ).toHaveBeenCalledTimes(1);
-    expect(mockSetAnswer).toHaveBeenCalledWith(['']);   
-  });     
+    expect(mockSetAnswer).toHaveBeenCalledWith(['']);
+  });
 });
