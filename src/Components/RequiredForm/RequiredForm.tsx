19ab0d65f63a02dac5d1180fb204401eed4e4368
Taylor Johnson <taylorjohnson141@gmail.com>
2021-01-14 15:43:34 -0700

    Fix enter on required form

--- src/Components/RequiredForm/RequiredForm.tsx
+++ src/Components/RequiredForm/RequiredForm.tsx
@@ -75,7 +75,7 @@ let RequiredForm = (props: RequiredFormProps) => {
     );
   }
   return (
-    <form>
+    <form onSubmit = {(e) => e.preventDefault()}>
     <p className='landing-page-instructions'>Welcome to Eras, the life calendar.  Eras allows you to holistically visualize your life in order to live more intentionally. We're going to ask you a few questions in order to get to know you, and generate your life calendar!  Let's start!</p>
       <div className='required-form'>
         <TextQuestion
