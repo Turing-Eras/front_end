957907787a019bd03b7cb251a47cdf25e54a5c30
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-26 20:21:42 -0700

    Write test for RequiredForm question

--- src/Components/RequiredForm/RequiredForm.test.tsx
+++ src/Components/RequiredForm/RequiredForm.test.tsx
@@ -16,6 +16,18 @@ describe('RequiredForm', () => {
     const userInstructions = getByText(
       'Welcome to Eras, the life calendar. Eras allows you to holistically visualize your life in order to live more intentionally. The form will ask you a few questions in order to get to know you and generate your life calendar! Lets start!'
     );
+
     expect(userInstructions).toBeInTheDocument();
+  });
+
+  it('should render a question', () => {
+    const { getByText } = render(
+      <MockedProvider>
+        <RequiredForm />
+      </MockedProvider>
+    );
+
+    const nameQuestion = getByText('What is your name?');
+    expect(nameQuestion).toBeInTheDocument();
   });
 });
